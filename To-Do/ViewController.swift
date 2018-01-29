d1c69dde2e81b5949b8e39187a9ee301927bd8c5
MohamedDer <medder@mohameds-macbook-pro.local>
2018-01-29 16:18:42 +0000

    Initial commit

--- To-Do/ViewController.swift
+++ To-Do/ViewController.swift
@@ -1,23 +1,39 @@
-//
 //  ViewController.swift
 //  To-Do
 //
 //  Created by Mohamed Derkaoui on 29/01/2018.
 //  Copyright © 2018 Mohamed Derkaoui. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var items:[String]=[]
+    
+    @IBOutlet weak var mTextField: UITextField!
+    @IBOutlet weak var mItemsLabel: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func addItem(_ sender: Any) {
+        if mTextField.text == "" {
+            return
+        }
+        items.append(mTextField.text!)
+        mItemsLabel.text=""
+        for item in items{
+            mItemsLabel.text.append(" - "+item+"\n")
+        }
+        mTextField.resignFirstResponder()
+        mTextField.text=""
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
